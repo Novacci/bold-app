300805d60cfebcf5aef79f65a1a0d0565d62a122
Wojciech Nowak <wojtekn00@gmail.com>
2023-07-03 16:20:41 +0200

    styled table and added pagination

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,5 +1,7 @@
 import persons from '../../sluzba.json';
 import './DataTable.scss';
+import { StylesProvider } from '@material-ui/core/styles';
+import useTableFunction from '../../hooks/useTableFunction';
 import {
   TableContainer,
   Table,
@@ -8,7 +10,10 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  TablePagination,
+  TableSortLabel,
 } from '@mui/material';
+import { useState } from 'react';
 
 interface Person {
   id: number;
@@ -20,31 +25,73 @@ interface Person {
 }
 
 export default function DataTable() {
+  const { page, rowsPerPage, handleChangeRowsPerPage, handleChangePage } =
+    useTableFunction();
+  const [order, setOrder] = useState<any>('asc');
+  const [orderBy, setOrderBy] = useState<keyof Person>();
+
+  // const handleSort = (property: keyof Person) => (event: any) => {
+  //   console.log(property);
+  //   const isAsc = orderBy === property && order === 'asc';
+  //   setOrder(isAsc ? 'desc' : 'asc');
+  //   setOrderBy(property);
+  // };
+
+  // const arraySort = (a: Person, b: Person) => {
+  //   if (order === 'asc') {
+  //     return a[orderBy] > b[orderBy] ? 1 : -1;
+  //   } else {
+  //     return a[orderBy] < b[orderBy] ? 1 : -1;
+  //   }
+  // };
+
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {Object.keys(persons[0]).map((field) => (
-                <TableCell>{field}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {persons.map((person) => (
+      <StylesProvider injectFirst>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead className="header">
               <TableRow>
-                <TableCell>{person.id}</TableCell>
-                <TableCell>{person.firstName}</TableCell>
-                <TableCell>{person.lastName}</TableCell>
-                <TableCell>{person.dateOfBirth}</TableCell>
-                <TableCell>{person.function}</TableCell>
-                <TableCell>{person.experience}</TableCell>
+                {Object.keys(persons[0]).map((field) => (
+                  <TableCell>
+                    <TableSortLabel
+
+                    // active={orderBy === ''}
+                    // direction={orderBy === '' ? order : 'asc'}
+                    // onClick={handleSort(Object.keys(persons[0]))}
+                    >
+                      {field}
+                    </TableSortLabel>
+                  </TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {persons
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((person) => (
+                  <TableRow>
+                    <TableCell>{person.id}</TableCell>
+                    <TableCell>{person.firstName}</TableCell>
+                    <TableCell>{person.lastName}</TableCell>
+                    <TableCell>{person.dateOfBirth}</TableCell>
+                    <TableCell>{person.function}</TableCell>
+                    <TableCell>{person.experience}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component={Paper}
+            count={persons.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </TableContainer>
+      </StylesProvider>
     </>
   );
 }
---
+++ src/hooks/useTableFunction.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function useTableFunction() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(event.target.value);
+    setPage(0);
+  };
+
+  return {
+    page,
+    rowsPerPage,
+    handleChangeRowsPerPage,
+    handleChangePage,
+  };
+}
